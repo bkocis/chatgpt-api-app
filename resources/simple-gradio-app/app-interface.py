fefe11e4caadceb7b8b3698d108a03d3e8a041c7
Balazs.Kocsis <balaz.kocis@gmail.com>
2023-06-18 16:17:57 +0200

    update run via makefile

--- resources/simple-gradio-app/app-interface.py
+++
@@ -1,8 +0,0 @@
-import gradio as gr
-
-
-def sketch_recognition(img):
-    pass  # Implement your sketch recognition model here...
-
-
-gr.Interface(fn=sketch_recognition, inputs="sketchpad", outputs="label").launch()
